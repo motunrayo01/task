c3aedcb0652721ef5907a125f3198c4dd62b60a5
motunrayo01 <okunnigaomolola@gmail.com>
2020-12-21 14:52:00 +0100

    commit

--- src/Demand.js
+++ src/Demand.js
@@ -8,15 +8,15 @@ class Demand extends Component {
         return ( 
             <div>
                 <div className="container-fluid demand">
-                   <div className="row">
-                       <div className="col-6  ">
+                   <div className="row ">
+                       <div className="col-sm-6  ">
                            <h1 className="one">In demand-talent<br/> on demand <i>&#8482;</i></h1>
                            <h1 className="two">upwork is how &#8482;</h1>
                            <h5>Hire proven pros with confidence using the world’s largest, remote talent platform.</h5>
                            <Button className="button1" variant="success">Get Started</Button>
                            <Button className="button2" variant="light">Watch Demo</Button>
                        </div>
-                       <div className="col-6 " >
+                       <div className="col-sm-6 " >
                            <img src= "https://res.cloudinary.com/morayo/image/upload/v1608324720/man-using-ipad-xlarge_pcldso.webp" alt="a man" width="100px"/ >
                        </div>
                        </div> 
